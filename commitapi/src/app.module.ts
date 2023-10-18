bf7130c9dd298b6abcb214f767801b3088d40d75
Rodrigo Perez <rodrigoperez0109@gmail.com>
2023-10-18 20:23:13 -0300

    module commits structure

--- commitapi/src/app.module.ts
+++ commitapi/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
-
+import { CommitsModule } from './commits/commits.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(`mongodb+srv://myAtlasDBUser:mongo2024@myatlasclusteredu.eyw7zzd.mongodb.net/`, {
+      dbName: 'test'
+    }),
+    CommitsModule],
 })
 export class AppModule {}
