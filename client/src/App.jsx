91961c8dce218b6cd17a0d11f8d4add62df42296
Rodrigo Perez <rodrigoperez0109@gmail.com>
2023-10-19 15:17:09 -0300

    vite and tailwind install and config

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <div className="w-full h-full">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full h-[300px]'>
+      <h1 className='text-white text-3xl'>Welcome to <strong className='text-blue-600'>commit</strong> explorer<span className='text-blue-600'>.</span></h1>
+    </div>
+  )
+}
+
+export default Header
