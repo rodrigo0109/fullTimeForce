4532baf6bd663644422250dad7347feff1097172
Rodrigo Perez <rodrigoperez0109@gmail.com>
2023-10-21 18:54:23 -0300

    fonsize avatar

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -7,8 +7,8 @@ const Profile = () => {
     //console.log("INFO", user)
   return (
     <div className="flex flex-row items-center">
-        <img className="2xl:w-10 sm:2-5 sm:h-5 2xl:h-10 rounded-full" src={user?.picture} alt="Rounded avatar"></img>
-        <span className="ml-2.5 text-white sm:text-xs">{user?.nickname}</span>
+        <img className="2xl:w-10 sm:w-5 sm:h-5 2xl:h-10 rounded-full" src={user?.picture} alt="Rounded avatar"></img>
+        <span className="ml-2.5 text-white sm:text-xs 2xl:text-lg">{user?.nickname}</span>
     </div>
   )
 }
